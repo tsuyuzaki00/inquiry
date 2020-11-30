6385019b98e1819efd7e7191a8813dd8aabcad29
tsuyuzakitatsuya <tsuyuzaki.tatsuya@engi-st.jp>
2020-11-30 17:03:37 +0900

    選択したオブジェクトをリスト一覧として表示

---
+++ lookSelectList.py
@@ -0,0 +1,33 @@
+from mainEdit import qt
+from PySide2 import QtWidgets, QtCore, QtGui
+import pymel.core as pm
+
+def selectKeyAttr():
+    window = QtWidgets.QWidget(qt.getMayaWindow())
+    window.setWindowFlags(QtCore.Qt.Window)
+    window.resize(300,500)
+    layout = QtWidgets.QVBoxLayout(window)
+    
+    widget = QtWidgets.QPlainTextEdit(window)
+
+    sels = pm.selected()
+
+    try:
+        widget.insertPlainText(sels[0] + " = [\n")
+    except:
+        pm.error('Nothing is selected')
+    else:
+        for sel in sels:    
+            widget.insertPlainText("\t'" + sel + "'\n")
+        widget.insertPlainText("\t]")
+        layout.addWidget(widget)
+
+        button = QtWidgets.QPushButton('print',window)
+        layout.addWidget(button)
+
+        button.clicked.connect(main)
+
+        window.show()
+
+def main():
+    selectKeyAttr()
